1fc72220472f0370f9ef8395441411a0ab43042e
Mohammad Hosein Mousavi <biomousavi@gmail.com>
2024-01-31 17:52:32 +0330

    chore: config eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
-import { useState } from 'react'
-import viteLogo from '/vite.svg'
+import { useState } from "react";
+import viteLogo from "/vite.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div >
-        <a href="https://vitejs.dev" target="_blank">
+      <div>
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
-        </a>
+        <a href="https://react.dev" target="_blank" rel="noreferrer"></a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
@@ -26,7 +25,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
